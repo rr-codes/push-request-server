c6223338f9904a4986119e051cc0ea8c4b1cc78f
Richard Robinson <rir@my.yorku.ca>
2020-11-17 04:43:41 -0500

    Updated AuthenticationController

--- Sources/App/Controllers/AuthenticationController.swift
+++ Sources/App/Controllers/AuthenticationController.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Vapor
+
+struct AuthenticationController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        let auth = routes.grouped("auth")
+        
+        let callbackGroup = auth.grouped("callback")
+        callbackGroup.get(use: callback)
+        
+        let requestGroup = auth.grouped("request")
+        requestGroup.get(use: request)
+    }
+    
+    func callback(req: Request) throws -> Response {
+        return Response(status: .ok)
+    }
+    
+    func request(req: Request) throws -> Response {
+        return Response(status: .ok)
+    }
+}
